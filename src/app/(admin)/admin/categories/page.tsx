997be1437952823b86850ab3bd5edecaad74ac49
KryssNa <krishna.kryss@gmail.com>
2025-01-11 08:20:22 +0545

    categories modified

---
+++ src/app/(admin)/admin/categories/page.tsx
@@ -0,0 +1,10 @@
+import CategoriesManagement from '@/components/dashboard/admin/categories/categoryManagement'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <CategoriesManagement />
+    </div>
+  )
+}
