0b91c807228b2aafa58af8898bc468399db666d3
KryssNa <krishna.kryss@gmail.com>
2025-01-11 08:20:33 +0545

    admin setting updated

--- src/app/(admin)/admin/settings/page.tsx
+++ src/app/(admin)/admin/settings/page.tsx
@@ -1,9 +1,10 @@
+import SecuritySettings from '@/components/layouts/dashboard/setting/settingPage'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-      setting
+      <SecuritySettings />
     </div>
   )
 }
