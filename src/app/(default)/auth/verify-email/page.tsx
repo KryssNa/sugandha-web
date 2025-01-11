ea0a3678e2007d14b14cbf8a60348f50ddb26000
KryssNa <krishna.kryss@gmail.com>
2025-01-11 08:21:34 +0545

    email verification

---
+++ src/app/(default)/auth/verify-email/page.tsx
@@ -0,0 +1,9 @@
+import VerifyEmail from '@/components/sections/auth/verifyemail/verifyEmail'
+
+export default function page() {
+    return (
+        <div>
+            <VerifyEmail />
+        </div>
+    )
+}
