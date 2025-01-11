cc2cc6421bd6138b45fecf6ebcc43c1ca7a0769b
KryssNa <krishna.kryss@gmail.com>
2025-01-11 08:27:14 +0545

    config for security

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,45 @@
 import type { NextConfig } from "next";
+const fs = require('fs');
+const path = require('path');
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['localhost'], // Add your image domains here
-    // If you also need to support production domain, add it here
-    // For example: ['localhost', 'your-production-domain.com']
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/uploads/products/images/**',
+      },
+    ],
+    unoptimized: true, // Disable image optimization
   },
+  serverRuntimeConfig: {
+    httpsOptions: {
+      key: fs.readFileSync(path.join(process.cwd(), 'certificates/localhost-key.pem')),
+      cert: fs.readFileSync(path.join(process.cwd(), 'certificates/localhost.pem')),
+    },
+  },
+  reactStrictMode: true,
+  webpack: (config, { dev }) => {
+    if (dev) {
+      // Enable polling in development for faster updates
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 300,
+      };
+    }
+    return config;
+  },
+  // ignoreBuildErrors: true,
+
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint errors
+  },
+  typescript: {
+    ignoreBuildErrors: true, // Ignore TypeScript errors
+  },
+  
 };
 
 export default nextConfig;
