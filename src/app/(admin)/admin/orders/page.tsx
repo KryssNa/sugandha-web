3c6a9bd991777569ea4b70875adf6c6f4d652877
KryssNa <krishna.kryss@gmail.com>
2025-01-26 08:02:16 +0545

    order page for admin

--- src/app/(admin)/admin/orders/page.tsx
+++ src/app/(admin)/admin/orders/page.tsx
@@ -1,9 +1,10 @@
+import OrdersPage from '@/components/dashboard/admin/orders/OrderPage'
 import React from 'react'
 
 export default function page() {
   return (
     <div>
-      orders
+      <OrdersPage />
     </div>
   )
 }
