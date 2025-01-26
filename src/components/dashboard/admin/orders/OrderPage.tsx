51685240281ea3d6f185b8a626878a691fd27599
KryssNa <krishna.kryss@gmail.com>
2025-01-26 08:03:49 +0545

    order

--- src/components/dashboard/admin/orders/OrderPage.tsx
+++ src/components/dashboard/admin/orders/OrderPage.tsx
@@ -1,23 +1,44 @@
 // pages/admin/orders.tsx
-
+"use client"
 import { useDashboardFilters } from "@/hooks/dashboard/useFilters";
 import { useOrders } from "@/hooks/dashboard/useOrders";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import OrdersManagement, { Order } from "./OrderManagement";
 import StatusUpdateModal from "./StatusUpdateModal";
+import { OrderService } from "@/services/order.service";
 
 const OrdersPage = () => {
     const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
     const router = useRouter();
+    const [totalOrders, setTotalOrders] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const [orders, setOrders] = useState<Order[]>([]);
     const {
-        orders,
-        loading,
+        // orders,
+        // loading,
         updateOrder,
         deleteOrder,
         filters: orderFilters,
         updateFilters
     } = useOrders();
+
+      useEffect(() => {
+        const fetchOrders = async () => {
+          const response = await OrderService.getAllOrders();
+    
+          if (response.success) {
+            setLoading(false);
+            setOrders(response.data.orders.orders);
+            setTotalOrders(response.data.orders.total);
+            setLoading(false);
+          };
+          setLoading(false);
+    
+          console.log(response);
+        };
+        fetchOrders();
+      }, []);
 
     const {
         filters: dashboardFilters,
