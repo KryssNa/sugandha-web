6deb63c0a5a0cf3ae6ea6fdcaa8459440d26c1a5
KryssNa <krishna.kryss@gmail.com>
2025-01-31 06:38:31 +0545

    activity logs

---
+++ src/app/(admin)/admin/activity-logs/page.tsx
@@ -0,0 +1,11 @@
+
+import LogsPage from '@/components/layouts/main/admin/logs/LogPage'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div>
+      <LogsPage />
+    </div>
+  )
+}
